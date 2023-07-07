b9bb07b8c63111094e50fb960492dcab61aa1d65
hsgs-comp-sci <84114576+get-wright@users.noreply.github.com>
2023-07-07 13:39:48 +0700

    Update Pymanager.bat

--- main/Pymanager.bat
+++ main/Pymanager.bat
@@ -4,7 +4,7 @@ if %errorlevel% neq 0 (
     echo Python not found. Downloading Python installer...
     powershell -Command "Invoke-WebRequest -OutFile python-3.9.7.exe https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe"
     echo Installing Python...
-    start /wait python-3.9.7.exe /quiet InstallAllUsers=1 PrependPath=1
+    start /wait python-3.9.7.exe InstallAllUsers=1 PrependPath=1
 )
 
 pip --version >nul 2>&1
@@ -17,3 +17,4 @@ if %errorlevel% neq 0 (
 
 echo Running setup.py...
 call python setup.py
+pause
