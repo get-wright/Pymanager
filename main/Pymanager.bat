9c05887d46e837a132e73d0e8323259974be21c9
hsgs-comp-sci <84114576+get-wright@users.noreply.github.com>
2023-07-07 13:05:17 +0700

    Add files via upload

--- main/Pymanager.bat
+++ main/Pymanager.bat
@@ -1,32 +1,19 @@
 @echo off
-python --version 
+where /q python
 if %errorlevel% neq 0 (
-    echo Python not found.
-    set /p installPython="Do you want to install Python? (y/n): "
-    if /i "%installPython%"=="y" (
-        echo Downloading Python installer...
-        powershell -Command "Invoke-WebRequest -OutFile python-3.9.7.exe https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe"
-        echo %errorlevel%
-        echo Installing Python...
-        call python-3.9.7.exe InstallAllUsers=1 PrependPath=1
-        echo Python installation complete.
-        cscript //nologo refreshenv.vbs
-    )
+    echo Python not found. Downloading Python installer...
+    powershell -Command "Invoke-WebRequest -OutFile python-3.9.7.exe https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe"
+    echo Installing Python...
+    start /wait python-3.9.7.exe /quiet InstallAllUsers=1 PrependPath=1
 )
 
-pip --version 
+where /q pip
 if %errorlevel% neq 0 (
-    echo pip not found.
-    set /p installPip="Do you want to install pip? (y/n): "
-    if /i "%installPip%"=="y" (
-        echo Downloading get-pip.py...
-        powershell -Command "Invoke-WebRequest -OutFile get-pip.py https://bootstrap.pypa.io/get-pip.py"
-        echo %errorlevel%
-        echo Installing pip...
-        call python get-pip.py
-        echo pip installation complete.
-        cscript //nologo refreshenv.vbs
-    )
+    echo pip not found. Downloading get-pip.py...
+    powershell -Command "Invoke-WebRequest -OutFile get-pip.py https://bootstrap.pypa.io/get-pip.py"
+    echo Installing pip...
+    python get-pip.py
 )
 
+echo Running setup.py...
 python setup.py
